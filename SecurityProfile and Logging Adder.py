e6ab12a46f3a3c96421f384a32687d0d529ba029
Jason <jtheism@gmail.com>
2019-05-03 15:52:04 -0500

    5/3/19

--- SecurityProfile and Logging Adder.py
+++ SecurityProfile and Logging Adder.py
@@ -1,7 +1,7 @@
 import requests
 import urllib3
 from xml.etree import ElementTree
-from tkinter.filedialog import asksaveasfilename
+from tkinter.filedialog import asksaveasfilename, askopenfilename
 import os
 
 urllib3.disable_warnings()
@@ -14,14 +14,14 @@ tree = ''
 print("\nThis script will output CLI set commands to add security-group and logging profiles to Palo Alto firewall "
       "security policies.\n")
 while True:
-    choice_s = input("Do you want to add security-group profiles to the policies? (y or n)\n")
+    choice_s = input("Do you want to add SECURITY-GROUP profiles to the policies? (y or n)\n")
     if choice_s not in ["y", "n"]:
         print("Please enter y or n.")
         continue
     else:
         break
 while True:
-    choice_l = input("Do you want to add log-forwarding profiles to the policies? (y or n)\n")
+    choice_l = input("Do you want to add LOG-FORWARDING profiles to the policies? (y or n)\n")
     if choice_l not in ["y", "n"]:
         print("Please enter y or n.")
         continue
@@ -33,9 +33,11 @@ log_set = None
 
 
 while xml_input not in ["1", "2"]:
-    xml_input = input("Select input source:\n\t1: File path   2: Connect to firewall API\n")
+    xml_input = input("Select input source:\n\t1: Import .xml config file.   2: Connect to firewall API\n")
     if xml_input == "1":
-        tree = ElementTree.parse(input("Enter file path:\n"))
+        tree = ElementTree.parse(askopenfilename(initialdir=os.path.expanduser("~/Desktop"),
+                                                    title="Select Palo Alto .xml Config File."))
+        # tree = ElementTree.parse(input("Enter file path:\n"))
         break
     elif xml_input == "2":
         firewall_ip = input("Enter firewall IP:\n")
@@ -52,25 +54,35 @@ while xml_input not in ["1", "2"]:
 if choice_s == "y":
     sec_grp_name = input("Enter security profile group name:\n")
     sec_set = []
-    for child in tree.iter('entry'):  # This is where we get the root of each node for the individual rules.
-        sec_set.append(child.attrib["name"])
 
+    # for child in tree.iter('entry'):  # This is where we get the root of each node for the individual rules.
+    #     sec_set.append(child.attrib["name"])
+    for elem in tree.iter(tag="security"):
+        for rules in elem:
+            for entry in rules:
+                sec_set.append(entry.attrib["name"])
+                # print(entry.attrib["name"])
 if choice_l == "y":
     logging_name = input("Enter log forwarding group name:\n")
     log_set = []
-    for child in tree.iter('entry'):  # This is where we get the root of each node for the individual rules.
-        log_set.append(child.attrib["name"])
+    # for child in tree.iter('entry'):  # This is where we get the root of each node for the individual rules.
+    #     log_set.append(child.attrib["name"])
 
+    for elem in tree.iter(tag="security"):
+        for rules in elem:
+            for entry in rules:
+                log_set.append(entry.attrib["name"])
+                # print(entry.attrib["name"])
 
-print("\nset cli scripting-mode on\nconfigure")
+# print("\nset cli scripting-mode on\nconfigure")
 set_cmds = ["\nset cli scripting-mode on\n\nconfigure\n\n"]
 if sec_set:
     for name in sec_set:
-        print(f'set rulebase security rules "{name}" profile-setting group "{sec_grp_name}"')
+        # print(f'set rulebase security rules "{name}" profile-setting group "{sec_grp_name}"')
         set_cmds.append(f'set rulebase security rules "{name}" profile-setting group "{sec_grp_name}"\n')
 if log_set:
     for name in log_set:
-        print(f'set rulebase security rules "{name}" log-setting "{logging_name}"')
+        # print(f'set rulebase security rules "{name}" log-setting "{logging_name}"')
         set_cmds.append(f'set rulebase security rules "{name}" log-setting "{logging_name}"\n')
 try:
     with open(asksaveasfilename(initialdir=os.path.expanduser("~/Desktop"),
