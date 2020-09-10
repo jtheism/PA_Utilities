4f128458f5cf12b666fb91bf599b8274c6b2bc1b
Jason <jtheism@gmail.com>
2020-09-10 11:19:27 -0500

    9/10/20 Updated for 9.1 with some changes to ZP and added CTD

--- Golden_Config.py
+++ Golden_Config.py
@@ -6,11 +6,12 @@ Tk().withdraw()  # Prevents tkinter window from opening
 
 models = {1: ["PA-220", 2100, 3990], 2: ["PA-820", 4150, 7885], 3: ["PA-850", 6500, 12350],
           4: ["PA-3220", 28500, 54150], 5: ["PA-3250", 42000, 79800], 6: ["PA-3260", 59000, 112100],
-          7: ["PA-5250", 142000, 269800], 8: ["PA-5260", 195000, 370500], 9: ["PA-5280", 195000, 370500],
-          10: ["PA-7050", 1450000, 2755000], 11: ["PA-7080", 2400000, 4560000], 12: ["VM-50", 1500, 2850],
-          13: ["VM-100", 7500, 14250], 14: ["VM-200", 7500, 14250], 15: ["VM-300", 15000, 28500],
-          16: ["VM-500", 30000, 57000], 17: ["VM-700", 60000, 114000], 18: ["VM-1000-HV", 15000, 28500],
-          19: ["PA-5220", 66500, 126350]}
+          7: ["PA-5220", 66500, 126350], 8: ["PA-5250", 142000, 269800], 9: ["PA-5260", 195000, 370500],
+          10: ["PA-5280", 195000, 370500], 11: ["PA-7050", 1450000, 2755000], 12: ["PA-7080", 2400000, 4560000],
+          13: ["VM-50", 1500, 2850], 14: ["VM-100", 7500, 14250], 15: ["VM-200", 7500, 14250],
+          16: ["VM-300", 15000, 28500], 17: ["VM-500", 30000, 57000], 18: ["VM-700", 60000, 114000],
+          19: ["VM-1000-HV", 15000, 28500]
+          }
 
 
 text_list = ["set deviceconfig", "set mgt-config", "set network profiles", "set zone", "set tag", "set profiles",
@@ -23,13 +24,13 @@ pano_cmds = []
 
 panos_ver = None
 while panos_ver not in ["1", "2"]:
-    panos_ver = input("Select which PAN-OS version:\n\t1. 8.1\n\t2. 9.0\n")
+    panos_ver = input("Select which PAN-OS version:\n\t1. 9.0\n\t2. 9.1\n")
 
 u_zone = input(f'Enter desired outside zone name:\n')
 t_zone = input(f'Enter desired inside zone name:\n')
 
 if panos_ver == "1":
-    with open("V8.1-GoldCfgSetCmds", "r") as cmds_file:
+    with open("V9.0-GoldCfgSetCmds", "r") as cmds_file:
         for line in cmds_file:
             if "**Untrust" in line:
                 cmds_list.append(line.replace("**Untrust", u_zone))
@@ -40,7 +41,7 @@ if panos_ver == "1":
 
 
 elif panos_ver == "2":
-    with open("V9.0-GoldCfgSetCmds", "r") as cmds_file:
+    with open("V9.1-GoldCfgSetCmds", "r") as cmds_file:
         for line in cmds_file:
             if "**Untrust" in line:
                 cmds_list.append(line.replace("**Untrust", u_zone))
@@ -70,8 +71,9 @@ if is_panorama == "y":
 while True:
     try:
         is_model = int(input("Model?\n\n1: PA-220    2: PA-820    3: PA-850\n4: PA-3220   5: PA-3250   6: PA-3260\n"
-                         "7: PA-5250   8: PA-5260   9: PA-5280   10: PA-7050   11: PA-7080\n12: VM-50   13: VM-100   "
-                         "14: VM-200   15: VM-300   16: VM-500   17: VM-700   18: VM-1000HV\n   19: PA-5220\n"))
+                             "7: PA-5220   8: PA-5250   9: PA-5260   10: PA-5280   11: PA-7050   12: PA-7080\n"
+                             "13: VM-50   14: VM-100   15: VM-200   16: VM-300   17: VM-500   "
+                             "18: VM-700   19: VM-1000HV\n"))
         break
     except ValueError:
         continue
